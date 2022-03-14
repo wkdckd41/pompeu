5c56463fa290b1efa1b0c923f879ce426c57e1fe
oseung12 <osungg@gmail.com>
2022-03-14 16:47:46 +0900

    .

---
+++ app/src/main/java/com/pompeu/dao/MemberDao.java
@@ -0,0 +1,33 @@
+package com.pompeu.dao;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Mapper;
+import com.pompeu.domain.Member;
+
+@Mapper
+public interface MemberDao {
+
+  int countAll();
+
+  List<Member> findAll();
+
+  int insert(Member member);
+
+  Member findByNo(int no);
+
+  int update(Member member);
+
+  int delete(int no);
+
+}
+
+
+
+
+
+
+
+
+
+
+
