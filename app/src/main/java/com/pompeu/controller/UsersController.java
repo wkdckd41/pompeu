d2f1b5467fe7b442f2e24b6af4aba3a2c4e61363
InfraWhale <gyqjajang@gmail.com>
2022-03-16 20:19:00 +0900

    .

--- app/src/main/java/com/pompeu/controller/UsersController.java
+++ app/src/main/java/com/pompeu/controller/UsersController.java
@@ -3,47 +3,47 @@ package com.pompeu.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.pompeu.dao.MemberDao;
-import com.pompeu.domain.Member;
+import com.pompeu.dao.UsersDao;
+import com.pompeu.domain.Users;
 
 @RestController 
 public class UsersController {
 
   // @Autowired
   // - 필드 선언부에 이 애노테이션을 붙여서 표시해 두면, 
-  //   Spring Boot가 MemberController 객체를 만들 때 MemberDao 구현체를 찾아 자동으로 주입한다. 
+  //   Spring Boot가 UsersController 객체를 만들 때 MemberDao 구현체를 찾아 자동으로 주입한다. 
   //
   @Autowired
-  MemberDao memberDao;
+  UsersDao usersDao;
 
-  @RequestMapping("/member/list")
+  @RequestMapping("/users/list")
   public Object list() {
-    return memberDao.findAll();
+    return usersDao.findAll();
   }
 
-  @RequestMapping("/member/add")
-  public Object add(Member member) {
-    return memberDao.insert(member);
+  @RequestMapping("/users/add")
+  public Object add(Users users) {
+    return usersDao.insert(users);
   }
 
 
-  @RequestMapping("/member/get")
+  @RequestMapping("/users/get")
   public Object get(int no) {
-    Member member = memberDao.findByNo(no);
-    if (member == null) {
+    Users users = usersDao.findByNo(no);
+    if (users == null) {
       return "";
     }
-    return member;
+    return users;
   }
 
-  @RequestMapping("/member/update")
-  public Object update(Member member) {
-    return memberDao.update(member);
+  @RequestMapping("/users/update")
+  public Object update(Users users) {
+    return usersDao.update(users);
   }
 
-  @RequestMapping("/member/delete")
+  @RequestMapping("/users/delete")
   public Object delete(int no) {
-    return memberDao.delete(no);
+    return usersDao.delete(no);
   }
 
 }
