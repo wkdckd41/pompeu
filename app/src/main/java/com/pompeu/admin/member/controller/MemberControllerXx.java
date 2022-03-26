6ec420908788593619d8a74c33c3007a387f17db
oseung12 <osungg@gmail.com>
2022-03-26 11:19:12 +0900

    admin Member

--- app/src/main/java/com/pompeu/admin/member/controller/MemberControllerXx.java
+++ app/src/main/java/com/pompeu/admin/member/controller/MemberControllerXx.java
@@ -1,30 +1,48 @@
 package com.pompeu.admin.member.controller;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import com.pompeu.admin.member.dao.MemberDao;
-import com.pompeu.admin.member.domain.Member;
+import com.pompeu.admin.member.dao.MemberDaoXx;
+import com.pompeu.admin.member.domain.MemberXx;
 
+//@RestController 
+public class MemberControllerXx {
 
-@RestController
-public class MemberController {
-
+  // @Autowired
+  // - 필드 선언부에 이 애노테이션을 붙여서 표시해 두면, 
+  //   Spring Boot가 MemberController 객체를 만들 때 MemberDao 구현체를 찾아 자동으로 주입한다. 
+  //
   @Autowired
-  MemberDao memberDao;
+  MemberDaoXx memberDao;
 
   @RequestMapping("/member/list")
   public Object list() {
     return memberDao.findAll();
   }
 
+  @RequestMapping("/member/add")
+  public Object add(MemberXx member) {
+    return memberDao.insert(member);
+  }
+
   @RequestMapping("/member/get")
   public Object get(int no) {
-    Member member = memberDao.findByNo(no);
+    MemberXx member = memberDao.findByNo(no);
     if (member == null) {
       return "";
     }
     return member;
+  }
+
+  @RequestMapping("/member/update")
+  public Object update(MemberXx member) {
+    return memberDao.update(member);
+  }
+
+  @RequestMapping("/member/delete")
+  public Object delete(int no) {
+    return memberDao.delete(no);
   }
 
   @RequestMapping("/member/findCount")
@@ -33,7 +51,7 @@ public class MemberController {
   }
 
   @RequestMapping("/member/srchMember")
-  public Object srchMember(Member member) {
+  public Object srchMember(MemberXx member) {
 
     System.out.println("srchMember : " + member.getNickName());
     System.out.println("srchMember : " + member.getPhone());
@@ -44,7 +62,7 @@ public class MemberController {
   }
 
   @RequestMapping("/member/memberRegi")
-  public Object memberRegi(Member member) {
+  public Object memberRegi(MemberXx member) {
 
     System.out.println("memberRegi : " + member.getName());
     System.out.println("memberRegi : " + member.getEmail());
--- app/src/main/java/com/pompeu/admin/member/domain/MemberXx.java
+++ app/src/main/java/com/pompeu/admin/member/domain/MemberXx.java
@@ -2,33 +2,32 @@ package com.pompeu.admin.member.domain;
 
 import java.sql.Date;
 
-
-
-public class Member {
+public class MemberXx {
 
   int no;
-  int memberTypeNo;
   String name;
   String email;
   String phone;
-  String birth;
   String nickName;
-  int gender;
-  int password;
-  Date joinDate;
-  Date loginDate;
-  Date modifyDate;
   int useCheck;
+  Date loginDate;
+  Date joinDate;
+  Date modifyDate;
   int adminCheck;
+  String password;
+  String birth;
+  int gender;
   int snsNo;
+  int memberTypeNo;
+
 
   @Override
   public String toString() {
-    return "Member [no=" + no + ", memberTypeNo=" + memberTypeNo + ", name=" + name + ", email="
-        + email + ", phone=" + phone + ", birth=" + birth + ", nickName=" + nickName + ", gender="
-        + gender + ", password=" + password + ", joinDate=" + joinDate + ", loginDate=" + loginDate
-        + ", modifyDate=" + modifyDate + ", useCheck=" + useCheck + ", adminCheck=" + adminCheck
-        + ", snsNo=" + snsNo + "]";
+    return "Member [no=" + no + ", name=" + name + ", email=" + email + ", phone=" + phone
+        + ", nickName=" + nickName + ", useCheck=" + useCheck + ", loginDate=" + loginDate
+        + ", joinDate=" + joinDate + ", modifyDate=" + modifyDate + ", adminCheck=" + adminCheck
+        + ", password=" + password + ", birth=" + birth + ", gender=" + gender + ", snsNo=" + snsNo
+        + ", memberTypeNo=" + memberTypeNo + "]";
   }
 
   public int getNo() {
@@ -36,12 +35,6 @@ public class Member {
   }
   public void setNo(int no) {
     this.no = no;
-  }
-  public int getMemberTypeNo() {
-    return memberTypeNo;
-  }
-  public void setMemberTypeNo(int memberTypeNo) {
-    this.memberTypeNo = memberTypeNo;
   }
   public String getName() {
     return name;
@@ -61,47 +54,11 @@ public class Member {
   public void setPhone(String phone) {
     this.phone = phone;
   }
-  public String getBirth() {
-    return birth;
-  }
-  public void setBirth(String birth) {
-    this.birth = birth;
-  }
   public String getNickName() {
     return nickName;
   }
   public void setNickName(String nickName) {
     this.nickName = nickName;
-  }
-  public int getGender() {
-    return gender;
-  }
-  public void setGender(int gender) {
-    this.gender = gender;
-  }
-  public int getPassword() {
-    return password;
-  }
-  public void setPassword(int password) {
-    this.password = password;
-  }
-  public Date getJoinDate() {
-    return joinDate;
-  }
-  public void setJoinDate(Date joinDate) {
-    this.joinDate = joinDate;
-  }
-  public Date getLoginDate() {
-    return loginDate;
-  }
-  public void setLoginDate(Date loginDate) {
-    this.loginDate = loginDate;
-  }
-  public Date getModifyDate() {
-    return modifyDate;
-  }
-  public void setModifyDate(Date modifyDate) {
-    this.modifyDate = modifyDate;
   }
   public int getUseCheck() {
     return useCheck;
@@ -109,11 +66,47 @@ public class Member {
   public void setUseCheck(int useCheck) {
     this.useCheck = useCheck;
   }
+  public Date getLoginDate() {
+    return loginDate;
+  }
+  public void setLoginDate(Date loginDate) {
+    this.loginDate = loginDate;
+  }
+  public Date getJoinDate() {
+    return joinDate;
+  }
+  public void setJoinDate(Date joinDate) {
+    this.joinDate = joinDate;
+  }
+  public Date getModifyDate() {
+    return modifyDate;
+  }
+  public void setModifyDate(Date modifyDate) {
+    this.modifyDate = modifyDate;
+  }
   public int getAdminCheck() {
     return adminCheck;
   }
   public void setAdminCheck(int adminCheck) {
     this.adminCheck = adminCheck;
+  }
+  public String getPassword() {
+    return password;
+  }
+  public void setPassword(String password) {
+    this.password = password;
+  }
+  public String getBirth() {
+    return birth;
+  }
+  public void setBirth(String birth) {
+    this.birth = birth;
+  }
+  public int getGender() {
+    return gender;
+  }
+  public void setGender(int gender) {
+    this.gender = gender;
   }
   public int getSnsNo() {
     return snsNo;
@@ -121,5 +114,17 @@ public class Member {
   public void setSnsNo(int snsNo) {
     this.snsNo = snsNo;
   }
+  public int getMemberTypeNo() {
+    return memberTypeNo;
+  }
+  public void setMemberTypeNo(int memberTypeNo) {
+    this.memberTypeNo = memberTypeNo;
+  }
+
+
+
+
+
+
 
 }
