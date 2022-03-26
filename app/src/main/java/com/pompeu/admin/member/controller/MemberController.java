9c80c3e2c3801e350818704d5d869703fa757313
InfraWhale <gyqjajang@gmail.com>
2022-03-26 12:09:54 +0900

    경로오류 수정

--- app/src/main/java/com/pompeu/admin/member/controller/MemberController.java
+++ app/src/main/java/com/pompeu/admin/member/controller/MemberController.java
@@ -4,17 +4,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.pompeu.admin.member.dao.MemberDao;
-import com.pompeu.admin.member.domain.Member;
+import com.pompeu.domain.Member;
 
-@RestController
+@RestController 
 public class MemberController {
 
+  // @Autowired
+  // - 필드 선언부에 이 애노테이션을 붙여서 표시해 두면, 
+  //   Spring Boot가 MemberController 객체를 만들 때 MemberDao 구현체를 찾아 자동으로 주입한다. 
+  //
   @Autowired
   MemberDao memberDao;
 
   @RequestMapping("/member/list")
   public Object list() {
     return memberDao.findAll();
+  }
+
+  @RequestMapping("/member/add")
+  public Object add(Member member) {
+    return memberDao.insert(member);
   }
 
   @RequestMapping("/member/get")
@@ -24,6 +33,16 @@ public class MemberController {
       return "";
     }
     return member;
+  }
+
+  @RequestMapping("/member/update")
+  public Object update(Member member) {
+    return memberDao.update(member);
+  }
+
+  @RequestMapping("/member/delete")
+  public Object delete(int no) {
+    return memberDao.delete(no);
   }
 
   @RequestMapping("/member/findCount")
