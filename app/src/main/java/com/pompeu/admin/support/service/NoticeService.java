9bfacdd1cddad8705b7e533d7a27045e0036f0ad
linarano <ranothing@gmail.com>
2022-03-29 20:33:17 +0900

    공지사항 add() 추가

---
+++ app/src/main/java/com/pompeu/admin/support/service/NoticeService.java
@@ -0,0 +1,18 @@
+package com.pompeu.admin.support.service;
+
+import java.util.List;
+import com.pompeu.domain.Notice;
+
+public interface NoticeService {
+
+  int add(Notice notice);
+
+  List<Notice> list();
+
+  Notice get(int no);
+
+  int update(Notice notice);
+
+  int delete(int no);
+}
+
