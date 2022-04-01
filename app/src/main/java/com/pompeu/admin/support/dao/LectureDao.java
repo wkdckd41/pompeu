dae520d67c4d7d91853b9e288b642072264a4489
sh1745 <ish794285@gmail.com>
2022-04-01 09:39:34 +0900

    lecture modified

--- app/src/main/java/com/pompeu/admin/support/dao/LectureDao.java
+++ app/src/main/java/com/pompeu/admin/support/dao/LectureDao.java
@@ -1,8 +1,10 @@
 package com.pompeu.admin.support.dao;
 
 import java.util.List;
+import org.apache.ibatis.annotations.Mapper;
 import com.pompeu.domain.Lecture;
 
+@Mapper
 public interface LectureDao {
 
 
