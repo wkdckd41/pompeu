7dbf892d6e4aac97de30471123ad503397a27e0a
InfraWhale <gyqjajang@gmail.com>
2022-04-02 00:00:02 +0900

    관리자-회원관리 서비스객체 도입

--- app/src/main/java/com/pompeu/admin/member/service/Impl/DefaultMemberService.java
+++ app/src/main/java/com/pompeu/admin/member/service/Impl/DefaultMemberService.java
@@ -1,5 +1,70 @@
 package com.pompeu.admin.member.service.Impl;
 
-public class DefaultMemberService {
+import java.util.List;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.pompeu.admin.member.dao.MemberDao;
+import com.pompeu.admin.member.service.MemberService;
+import com.pompeu.domain.Member;
+
+@Service
+public class DefaultMemberService implements MemberService{
+
+  @Autowired
+  MemberDao memberDao;
+
+  @Override
+  public List<Member> list() {
+    return memberDao.findAll();
+  }
+
+  @Override
+  public int add(Member member) {
+    return memberDao.insert(member);
+  }
+
+  @Override
+  public Member get(int no) {
+    return memberDao.findByNo(no);
+  }
+
+  @Override
+  public int update(Member member) {
+    return memberDao.update(member);
+  }
+
+
+  @Override
+  public int delete(int no) {
+    return memberDao.delete(no);
+  }
+
+  @Override
+  public List<Integer> memberList() {
+    return memberDao.findCount();
+  }
+
+  @Override
+  public List<Member> srchMember(Member member) {
+    return memberDao.srchMember(member);
+  }
+
+  @Override
+  public List<Map<Object, Object>> getClass(int no) {
+    return memberDao.findUserClass(no);
+  }
+
+  @Override
+  public List<Map<Object, Object>> creatorClass(int no) {
+    return memberDao.findCreatorClass(no);
+  }
+
+  @Override
+  public List<Map<Object, Object>> applyingClass(int no) {
+    return memberDao.findApplyingClass(no);
+  }
+
+
 
 }
