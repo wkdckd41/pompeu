fa214b3704ebc55914e49e2e810ab79df5959f8f
linarano <ranothing@gmail.com>
2022-04-03 01:24:16 +0900

    lecturetime add()수정

--- app/src/main/java/com/pompeu/creator/lecture/controller/LectureController.java
+++ app/src/main/java/com/pompeu/creator/lecture/controller/LectureController.java
@@ -1,10 +1,13 @@
 package com.pompeu.creator.lecture.controller;
 
+
+import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.pompeu.creator.lecture.Service.LectureService;
 import com.pompeu.domain.Lecture;
+import com.pompeu.domain.LectureTime;
 
 @RestController
 public class LectureController {
@@ -19,8 +22,20 @@ public class LectureController {
   }
 
   @RequestMapping("/lecture/add")
-  public Object add(Lecture lecture) {
-    System.out.println(this.lectureService);
+  public Object add(Lecture lecture, String[] times) {
+
+    ArrayList<LectureTime> timesList = new ArrayList<>();
+    for(int i= 0; i< times.length; i++) {
+      String[] value = times[i].split(",");
+      if(value[1].length() == 0) {
+        continue;
+      }
+      LectureTime lectureTime = new LectureTime
+          (value[2],value[3],Integer.parseInt(value[4]));
+
+      timesList.add(lectureTime);
+    }
+    lecture.setTimes(timesList);
     return lectureService.add(lecture);
   }
 
