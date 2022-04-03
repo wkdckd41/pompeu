1f3b06617c2b7080d1798caa6d981b2f081e5e36
wkdckd41 <wkdckd41@gmail.com>
2022-04-04 00:32:50 +0900

    notice write js 수정
    LectureController.java
    LectureDao.java
    LectureDao.xml 수정

--- app/src/main/java/com/pompeu/creator/lecture/controller/LectureController.java
+++ app/src/main/java/com/pompeu/creator/lecture/controller/LectureController.java
@@ -22,16 +22,19 @@ public class LectureController {
   }
 
   @RequestMapping("/lecture/add")
-  public Object add(Lecture lecture, String[] times) {
-
+  public Object add(Lecture lecture, String[] time) {
     ArrayList<LectureTime> timesList = new ArrayList<>();
-    for(int i= 0; i< times.length; i++) {
-      String[] value = times[i].split(",");
-      if(value[1].length() == 0) {
+    for(int i= 0; i< time.length; i++) {
+      String[] value = time[i].split(",");
+      if(value[0].length() == 0) { //시작시간
         continue;
-      }
+      } 
+      if(value[1].length() == 0) { //종료시간
+        continue;
+      }  
+
       LectureTime lectureTime = new LectureTime
-          (value[2],value[3],Integer.parseInt(value[4]));
+          (value[0],value[1],Integer.parseInt(value[2]));
 
       timesList.add(lectureTime);
     }
