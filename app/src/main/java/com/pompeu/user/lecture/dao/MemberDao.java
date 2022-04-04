f2cd70017e4c8461b9c55c398c56e6eeef8372f9
Park Ryuck <pryeok@gmail.com>
2022-04-04 15:21:33 +0900

    user.lecture

---
+++ app/src/main/java/com/pompeu/user/lecture/dao/MemberDao.java
@@ -0,0 +1,34 @@
+package com.pompeu.user.lecture.dao;
+
+import java.util.List;
+import java.util.Map;
+import org.apache.ibatis.annotations.Mapper;
+import com.pompeu.domain.Member;
+
+@Mapper
+public interface MemberDao {
+
+  int countAll();
+
+  List<Member> findAll();
+
+  int insert(Member member);
+
+  Member findByNo(int no);
+
+  int update(Member member);
+
+  int delete(int no);
+
+  List<Integer> findCount();
+
+  List<Member> srchMember(Member member);
+
+  List<Map<Object, Object>> findUserLecture(int no);
+
+  List<Map<Object, Object>> findCreatorLecture(int no);
+
+  List<Map<Object, Object>> findApplyingLecture(int no);
+
+}
+
---
+++ app/src/main/java/com/pompeu/user/party/service/Impl/DefaultMemberService.java
@@ -0,0 +1,70 @@
+package com.pompeu.user.party.service.Impl;
+
+import java.util.List;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.pompeu.admin.member.dao.MemberDao;
+import com.pompeu.admin.member.service.MemberService;
+import com.pompeu.domain.Member;
+
+@Service
+public class DefaultMemberService implements MemberService{
+
+  @Autowired
+  MemberDao memberDao;
+
+  @Override
+  public List<Member> list() {
+    return memberDao.findAll();
+  }
+
+  @Override
+  public int add(Member member) {
+    return memberDao.insert(member);
+  }
+
+  @Override
+  public Member get(int no) {
+    return memberDao.findByNo(no);
+  }
+
+  @Override
+  public int update(Member member) {
+    return memberDao.update(member);
+  }
+
+
+  @Override
+  public int delete(int no) {
+    return memberDao.delete(no);
+  }
+
+  @Override
+  public List<Integer> memberList() {
+    return memberDao.findCount();
+  }
+
+  @Override
+  public List<Member> srchMember(Member member) {
+    return memberDao.srchMember(member);
+  }
+
+  @Override
+  public List<Map<Object, Object>> getLecture(int no) {
+    return memberDao.findUserLecture(no);
+  }
+
+  @Override
+  public List<Map<Object, Object>> creatorLecture(int no) {
+    return memberDao.findCreatorLecture(no);
+  }
+
+  @Override
+  public List<Map<Object, Object>> applyingLecture(int no) {
+    return memberDao.findApplyingLecture(no);
+  }
+
+
+
+}
