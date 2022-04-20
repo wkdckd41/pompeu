56b4f23455e4e7c145b3b8fb75e0f798d50eae15
Park <pryeok@gmail.com>
2022-04-20 21:34:33 +0900

    user.lecture

--- app/src/main/resources/static/user/lecture/lectureHome.js
+++ app/src/main/resources/static/user/lecture/lectureHome.js
@@ -18,72 +18,37 @@ $(window).load(function() {
 });
 
 
-  var btnEverything = document.getElementById("btn-everything");
-
-  btnEverything.addEventListener('click', function() {
-  
-  var exName = document.querySelector("#exercise");
-  var name = document.querySelector("#lecture-name");
-  var lecturePrice  = document.querySelector("#lecture-price");
-  var sort = document.querySelector("#inputTag").value; 
-  
-  
-  console.log("exname : " + exName);
-  console.log("name : " + name);
-  console.log("price : " + lecturePrice);
-    
-  fetch(`/userLecture/findEverything?sort=${sort}`)
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(result) {
-      console.log("AAA");
-      console.log(result);
-      
-      $("#class-box").html(htmlGenerator(result));
-})
+var btnEverything = document.getElementById("btn-everything");
+btnEverything.addEventListener('click', function () {
+  loadList();
 })
 
 
   var btnOut = document.getElementById("btn-out");
-
 btnOut.addEventListener('click', function() {
-  
-  var exName = document.querySelector("#exercise");
-  var name = document.querySelector("#lecture-name");
-  var lecturePrice = document.querySelector("#lecture-price");
-  
-  console.log("exname : " + exName);
-  console.log("name : " + name);
-  console.log("price : " + lecturePrice);
-  
-  fetch(`/userLecture/findOut`)
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(result) {
-      console.log("AAA");
-      console.log(result);
-      
-      $("#class-box").html(htmlGenerator(result));
-      
-})
+  loadList();
 })
 
 
   var btnIn = document.getElementById("btn-in");
-
 btnIn.addEventListener('click', function() {
+  loadList();
+})
+
+function loadList() {
+  var inOut = document.querySelector("input[name=inout]:checked").value;
+  var sort = document.querySelector("#inputTag").value;
   
-  var exName = document.querySelector("#exercise");
-  var name = document.querySelector("#lecture-name");
-  var lecturePrice = document.querySelector("#lecture-price");
+  console.log(inOut, sort);
   
-  console.log("exname : " + exName);
-  console.log("name : " + name);
-  console.log("price : " + lecturePrice);
+  var url = "/userLecture/findEverything";
+  if (inOut == "in") {
+    url = "/userLecture/findIn";
+  } else if (inOut == "out") {
+    url = "/userLecture/findOut";
+  }
   
-  fetch(`/userLecture/findIn`)
+  fetch(`${url}?sort=${sort}`)
     .then(function(response) {
       return response.json();
     })
@@ -92,18 +57,17 @@ btnIn.addEventListener('click', function() {
       console.log(result);
       
       $("#class-box").html(htmlGenerator(result));
-
-})
-})
-
+    })
+}
 
   $("#btn-everything").click();
 
 
-
-
   function moveView(no) {
   location.href = 'lecture-detail.html?no='+no
+  
+
+
 }
 
 
