d80bcb27c0fabc2a7688b082e70fb1053420efec
oseung12 <osungg@gmail.com>
2022-04-21 19:53:53 +0900

    party home 지역별 조회

--- app/src/main/resources/static/user/party/partyHome.js
+++ app/src/main/resources/static/user/party/partyHome.js
@@ -12,12 +12,27 @@ var htmlGenerator = Handlebars.compile(trTemplate.innerHTML);
 document.querySelector("input[name=fruit][value=all]").checked = true;
 loadList();
 
+function onClickRegion(e) {
+  console.log(e.currentTarget);
+  var selectedRegion = document.querySelector(".region-selected");
+  if (selectedRegion != undefined) {
+    selectedRegion.classList.remove("region-selected");
+  }
+  e.currentTarget.classList.add("region-selected");
+  loadList();
+}
 
 function loadList() {
+  var region = "all";
+  var selectedRegion = document.querySelector(".region-selected div");
+  if (selectedRegion != undefined) {
+    region = selectedRegion.innerHTML;
+  }
   var sort = document.querySelector("#inputTag").value;
   var inOutEx = document.querySelector("input[name=fruit]:checked").value;
-  console.log(sort,inOutEx);
-    fetch(`/userparty/list?sort=${sort}&inOutEx=${inOutEx}`)
+  console.log(region,sort,inOutEx);
+  
+    fetch(`/userparty/list?sort=${sort}&inOutEx=${inOutEx}&region=${region}`)
     .then(function(response) {
       return response.json();
     })
