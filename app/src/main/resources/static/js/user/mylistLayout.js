7f4cdb85fad30e92fac39bc2e1ef97a456b4393d
sh1745 <ish794285@gmail.com>
2022-04-26 20:14:23 +0900

    mylist layout add

---
+++ app/src/main/resources/static/js/user/mylistLayout.js
@@ -0,0 +1,5 @@
+// $('head').append($('<link rel="stylesheet"/>').attr('href', '/css/user_common.css'));
+// $('head').append($('<link rel="stylesheet"/>').attr('href', '/user/login/layout.css'));
+$("#header").load("/form/user/header.html");
+$("#sidebar").load("/form/mypage/sidebar.html");
+$("#footer").load("/form/user/footer.html");
