d9cc7044d2e843940906aa8a332770596c28fe05
InfraWhale <gyqjajang@gmail.com>
2022-04-28 18:21:53 +0900

    크리에이터 정보수정기능 구현 완료

--- app/src/main/java/com/pompeu/creator/creatorChange/dao/CreatorChangeDao.java
+++ app/src/main/java/com/pompeu/creator/creatorChange/dao/CreatorChangeDao.java
@@ -1,34 +1,35 @@
 package com.pompeu.creator.creatorChange.dao;
 
 import java.util.List;
-import java.util.Map;
+
 import org.apache.ibatis.annotations.Mapper;
+
 import com.pompeu.domain.CreatorUpdate;
 import com.pompeu.domain.Member;
 
 @Mapper
 public interface CreatorChangeDao {
 
-  int countAll();
+	int countAll();
 
-  List<Member> findAll();
+	List<Member> findAll();
 
-  int insert(Member member);
+	int insert(Member member);
 
-  Member findByNo(int no);
+	Member findByNo(int no);
 
-  int update(Member member);
+	int update(Member member);
 
-  int delete(int no);
+	int delete(int no);
 
-  Map<Object,Object> getCreator(int no);
+	CreatorUpdate getCreator(int no);
 
-  int updateCreator(CreatorUpdate cUpdate);
+	int updateCreator(CreatorUpdate cUpdate);
 
-  int checkNickname(String nickname);
+	int checkNickname(String nickname);
 
-  int deleteCreator(Member member);
+	int deleteCreator(Member member);
 
-  int deleteCreatorDetail(int no);
+	int deleteCreatorDetail(int no);
 }
 
--- app/src/main/java/com/pompeu/creator/creatorChange/service/Impl/DefaultCreatorChangeService.java
+++ app/src/main/java/com/pompeu/creator/creatorChange/service/Impl/DefaultCreatorChangeService.java
@@ -1,9 +1,10 @@
 package com.pompeu.creator.creatorChange.service.Impl;
 
 import java.util.List;
-import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import com.pompeu.creator.creatorChange.dao.CreatorChangeDao;
 import com.pompeu.creator.creatorChange.service.CreatorChangeService;
 import com.pompeu.domain.CreatorUpdate;
@@ -12,58 +13,58 @@ import com.pompeu.domain.Member;
 @Service
 public class DefaultCreatorChangeService implements CreatorChangeService{
 
-  @Autowired
-  CreatorChangeDao creatorChangeDao;
+	@Autowired
+	CreatorChangeDao creatorChangeDao;
 
-  @Override
-  public List<Member> list() {
-    return creatorChangeDao.findAll();
-  }
+	@Override
+	public List<Member> list() {
+		return creatorChangeDao.findAll();
+	}
 
-  @Override
-  public int add(Member member) {
-    return creatorChangeDao.insert(member);
-  }
+	@Override
+	public int add(Member member) {
+		return creatorChangeDao.insert(member);
+	}
 
-  @Override
-  public Member get(int no) {
-    return creatorChangeDao.findByNo(no);
-  }
+	@Override
+	public Member get(int no) {
+		return creatorChangeDao.findByNo(no);
+	}
 
-  @Override
-  public int update(Member member) {
-    return creatorChangeDao.update(member);
-  }
+	@Override
+	public int update(Member member) {
+		return creatorChangeDao.update(member);
+	}
 
 
-  @Override
-  public int delete(int no) {
-    return creatorChangeDao.delete(no);
-  }
+	@Override
+	public int delete(int no) {
+		return creatorChangeDao.delete(no);
+	}
 
-  @Override
-  public Map<Object,Object> getCreator(int no) {
-    return creatorChangeDao.getCreator(no);
-  }
+	@Override
+	public CreatorUpdate getCreator(int no) {
+		return creatorChangeDao.getCreator(no);
+	}
 
-  @Override
-  public int updateCreator(CreatorUpdate cUpdate) {
-    return creatorChangeDao.updateCreator(cUpdate);
-  }
+	@Override
+	public int updateCreator(CreatorUpdate cUpdate) {
+		return creatorChangeDao.updateCreator(cUpdate);
+	}
 
-  @Override
-  public int checkNickname(String nickname) {
-    return creatorChangeDao.checkNickname(nickname);
-  }
+	@Override
+	public int checkNickname(String nickname) {
+		return creatorChangeDao.checkNickname(nickname);
+	}
 
-  @Override
-  public int deleteCreator(Member member) {
-    return creatorChangeDao.deleteCreator(member);
-  }
+	@Override
+	public int deleteCreator(Member member) {
+		return creatorChangeDao.deleteCreator(member);
+	}
 
-  @Override
-  public int deleteCreatorDetail(int no) {
-    return creatorChangeDao.deleteCreatorDetail(no);
-  }
+	@Override
+	public int deleteCreatorDetail(int no) {
+		return creatorChangeDao.deleteCreatorDetail(no);
+	}
 
 }
