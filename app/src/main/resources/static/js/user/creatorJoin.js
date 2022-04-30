a349baba389a6cfe63c6e9fc1cdcc7286d61f089
sh1745 <ish794285@gmail.com>
2022-05-01 01:16:08 +0900

    login modified

--- app/src/main/resources/static/js/user/creatorJoin.js
+++ app/src/main/resources/static/js/user/creatorJoin.js
@@ -56,36 +56,36 @@ const xCreatorBtn = document.querySelector('.creator_submit');
 
 
 
-// let nicknameCheck = nickNameBtn.addEventListener('click', function (e) {
-//     e.stopPropagation();
-//     fetch(`/user/nickcheck?nickname=${xNickName.value}`)
-//         .then((response) => response.text())
-//         .then((data) => {
-//             console.log(data);
-//             if (data == 'success') {
+let nicknameCheck = nickNameBtn.addEventListener('click', function (e) {
+    e.stopPropagation();
+    fetch(`/user/nickcheck?nickname=${xNickName.value}`)
+        .then((response) => response.text())
+        .then((data) => {
+            console.log(data);
+            if (data == 'success') {
 
-//                 Swal.fire({
-//                     icon: 'success',
-//                     title: '사용가능한 닉네임입니다.',
-//                 });
-//                 nickNameBtn.value = 'Y';
-//                 $("#memberEmail").focus();
-//             } else {
-//                 Swal.fire({
-//                     icon: 'warning',
-//                     title: '이미 사용중인 닉네임입니다.',
-//                 });
-//                 const input = xNickName.focus();
-//             }
+                Swal.fire({
+                    icon: 'success',
+                    title: '사용가능한 닉네임입니다.',
+                });
+                nickNameBtn.value = 'Y';
+                $("#memberEmail").focus();
+            } else {
+                Swal.fire({
+                    icon: 'warning',
+                    title: '이미 사용중인 닉네임입니다.',
+                });
+                const input = xNickName.focus();
+            }
 
-//         })
-// });
+        })
+});
 
 emailBtn.addEventListener('click', function (e) {
 
     const emailRule = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
 
-    if ($("#nicknameCheck").val() != 'Y') {
+    if ($(".nickNameBtn").val() != 'Y') {
         Swal.fire({
             icon: 'warning',
             title: '닉네임 중복확인을 해주세요.',
