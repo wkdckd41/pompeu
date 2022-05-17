3db695e5ee1ef5b70063611c38a1f8e29b6dcffb
pryeok <pryeok@gmail.com>
2022-05-17 21:20:46 +0900

    login

--- app/src/main/java/com/pompeu/security/service/PrincipalDetailService.java
+++ app/src/main/java/com/pompeu/security/service/PrincipalDetailService.java
@@ -24,10 +24,6 @@ public class PrincipalDetailService implements UserDetailsService {
   //  PasswordEncoder passwordEncoder;
 
 
-
-
-
-
   //시큐리티 설정에서 loginProcessingUrl();
   //login 요청시 자동으로 UserDetatilsService 타입으로 ioc되어 있는 loadUserByUsername 함수가 실행
 
